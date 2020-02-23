3f2d572aeef46510d94b14c2754e26aa1b08d507
Shaye Ulman <shayeulman@gmail.com>
2020-02-23 14:01:42 +0200

    changed SM preview images

---
+++ src/components/RedditCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <g-link :to="`https://www.reddit.com/r/${channel}`">
+    <h1>r/{{ channel }}</h1>
+  </g-link>
+</template>
+
+<script>
+export default {
+  props: {
+    channel: String,
+    color: String
+  }
+};
+</script>
--- src/pages/reddit.vue
+++ src/pages/reddit.vue
@@ -1,5 +1,22 @@
 <template>
   <Layout>
-    reddit
+    <reddit-card
+      v-for="(cahnnel, i) in channels"
+      :key="i"
+      :channel="channels.channel"
+      :color="channels.color"
+    />
   </Layout>
 </template>
+
+<script>
+import RedditCard from "~/components/RedditCard";
+export default {
+  components: { RedditCard },
+  data() {
+    return {
+      channels: [{ name: "vuejs", color: "green" }]
+    };
+  }
+};
+</script>
