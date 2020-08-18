b4414870af6ad9003c797c454ac873321644a822
Shaya Ulman <shayaulman@gmail.com>
2020-08-18 11:23:42 +0300

    fix + format

--- data/podcasts.js
+++ data/podcasts.js
@@ -1,16 +1,15 @@
-@@ -1,29 +1,36 @@
 module.exports = [
   {
     name: "מפתחים חסרי תרבות",
     description: "פודקאסט בעברית על חיי היום יום של צוותי פיתוח",
     link: "http://notarbut.co/",
-    color: "#919DCC"
+    color: "#919DCC",
   },
   {
     name: "רברס עם פלטפורמה",
     description: "הפודקאסט של אורי ורן",
     link: "https://www.reversim.com",
-    color: "#ff2600"
+    color: "#ff2600",
   },
   {
     name: "Changelog",
@@ -18,20 +17,22 @@ module.exports = [
     link: "https://changelog.com",
     image:
       "https://cdn.changelog.com/images/brand/changelog-logomark-97913322799ebebe4ec08d1ee2492899.svg?vsn=d",
-    color: "#3d765f"
+    color: "#3d765f",
   },
   {
     name: "Syntax",
     description: "A Tasty Treats Podcast for Web Developers.",
     link: "https://syntax.fm",
     image: "syntaxfm.png",
-    color: "#d0a650"
+    color: "#d0a650",
   },
   {
     name: "Code[ish]",
-    description: "A podcast from the team at Heroku, exploring code, technology, tools, tips, and the life of the developer.",
+    description:
+      "A podcast from the team at Heroku, exploring code, technology, tools, tips, and the life of the developer.",
     link: "https://www.heroku.com/podcasts/codeish",
-    image: "https://www1.assets.heroku.com/assets/podcast/codeish/codeish-logo-square-purple-16a8ad4d7ee52d1e31726be3527a299057df336503828eadb48d595a5c8ad5e8.svg",
-    color: "#79589F"
-  }
+    image:
+      "https://www1.assets.heroku.com/assets/podcast/codeish/codeish-logo-square-purple-16a8ad4d7ee52d1e31726be3527a299057df336503828eadb48d595a5c8ad5e8.svg",
+    color: "#79589F",
+  },
 ];
