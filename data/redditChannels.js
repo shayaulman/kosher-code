f4fbbe27b24bd22919748b7a368160580121431e
Yossi Zahn <yossizahn@gmail.com>
2020-09-14 00:01:11 +0300

    Add the aws subreddit

--- data/redditChannels.js
+++ data/redditChannels.js
@@ -20,4 +20,5 @@ module.exports = [
   { name: "Chrome", color: "#d54345" },
   { name: "PostgreSQL", color: "#014980" },
   { name: "laravel", color: "#ff2d20" },
+  { name: "aws", color: "#146eb4" },
 ];
