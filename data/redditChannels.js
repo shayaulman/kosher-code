9049acc532946e9a76076315855fe1f60c288e2f
yossizahn <yossizahn@gmail.com>
2020-09-30 08:59:27 +0000

    Add PHPhelp subreddit

--- data/redditChannels.js
+++ data/redditChannels.js
@@ -28,4 +28,5 @@ module.exports = [
   { name: "Angular2", color: "#e4e8ea" },
   { name: "angularjs", color: "#0079D3" },
   { name: "cscareerquestions", color: "#349e48" },
+  { name: "PHPhelp", color: "#8f9ed1" },
 ];
