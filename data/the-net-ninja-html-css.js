8a0182d576626c7467f1de8cf7083b7107a1ede8
meirlamdan <meirlamdan@users.noreply.github.com>
2021-12-24 14:28:57 +0200

    add the-net-ninja-html-css.js

---
+++ data/the-net-ninja-html-css.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    name: 'The Net Ninja - HTML & CSS',
+    hebrewName: '',
+    category: 'playlist',
+    tags: ['html', 'css'],
+    videoTutorials: [
+      { url: 'hu-q2zYwEYs' },
+      { url: 'mbeT8mpmtHA' },
+      { url: 'YwbIeMlxZAU' },
+      { url: 'D3iEE29ZXRM' },
+      { url: 'FHZn6706e3Q' },
+      { url: 'kGW8Al_cga4' },
+      { url: '25R1Jl5P7Mw' },
+      { url: 'XQaHAAXIVg8' },
+      { url: 'FMu2cKWD90g' },
+      { url: 'Xig7NsIE6DI' }
+    ]
+  }
+];
+https://www.youtube.com/watch?v=mbeT8mpmtHA
+https://www.youtube.com/watch?v=YwbIeMlxZAU
+https://www.youtube.com/watch?v=D3iEE29ZXRM
+https://www.youtube.com/watch?v=FHZn6706e3Q
+https://www.youtube.com/watch?v=kGW8Al_cga4
+https://www.youtube.com/watch?v=25R1Jl5P7Mw
+https://www.youtube.com/watch?v=XQaHAAXIVg8
+https://www.youtube.com/watch?v=FMu2cKWD90g
+https://www.youtube.com/watch?v=Xig7NsIE6DI
