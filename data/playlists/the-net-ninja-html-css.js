7bba9238a2a3ec2c18328b7f6d78114926994138
meir lamdan <meirlamdan@users.noreply.github.com>
2021-12-24 15:34:06 +0200

    add the-net-ninja-html-css.js

---
+++ data/playlists/the-net-ninja-html-css.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    name: 'The Net Ninja - HTML & CSS',
+    hebrewName: '',
+    category: 'playlist',
+    tags: ['html', 'css'],
+    videoTutorials: [
+      { url: 'hu-q2zYwEYs' },
+      { url: 'mbeT8mpmtHA' },
+      { url: 'YwbIeMlxZAU' },
+      { url: 'D3iEE29ZXRM' },
+      { url: 'FHZn6706e3Q' },
+      { url: 'kGW8Al_cga4' },
+      { url: '25R1Jl5P7Mw' },
+      { url: 'XQaHAAXIVg8' },
+      { url: 'FMu2cKWD90g' },
+      { url: 'Xig7NsIE6DI' }
+    ]
+  }
+];
