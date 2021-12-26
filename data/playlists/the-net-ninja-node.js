4aa788c7fd7169dbe1b624f8a1cb541c0c91883a
meir lerner <mchlerner@gmail.com>
2021-12-26 14:31:36 +0200

    organizing videos to playlist

---
+++ data/playlists/the-net-ninja-node.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    name: 'The Net Ninja - Node.js Crash Course',
+    hebrewName: '',
+    category: 'playlist',
+    tags: ['NodeJS'],
+    videoTutorials: [
+      { url: 'zb3Qk8SG5Ms' },
+      { url: 'OIBIXYLJjsI' },
+      { url: '-HPZ1leCV8k' },
+      { url: 'DQD00NAUPNk' },
+      { url: 'bdHE2wHT-gQ' },
+      { url: 'Lr9WUkeYSA8' },
+      { url: 'yXEesONd_54' },
+      { url: '_GJKAs7A0_4' },
+      { url: 'bxsemcrY4gQ' },
+      { url: 'VVGgacjzc2Y' },
+      { url: 'zW_tZR0Ir3Q' }
+    ]
+  }
+];
