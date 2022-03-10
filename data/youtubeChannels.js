f1fe24e4d8fce134e5e43c446da50d76b7c03cd1
Shaya Ulman <shayaulman@gmail.com>
2022-03-11 00:22:00 +0200

    data: YT open channels list

---
+++ data/youtubeChannels.js
@@ -0,0 +1,10 @@
+// Source: https://tchumim.com/topic/12462/%D7%91%D7%A7%D7%A9%D7%94-%D7%9C%D7%94%D7%9E%D7%9C%D7%A6%D7%95%D7%AA-%D7%A2%D7%9C-%D7%A2%D7%A8%D7%95%D7%A6%D7%99%D7%9D-%D7%A1%D7%A8%D7%98%D7%95%D7%A0%D7%99%D7%9D-%D7%A9%D7%9C-%D7%AA%D7%9B%D7%A0%D7%95%D7%AA-%D7%95%D7%94%D7%9E%D7%A1%D7%AA%D7%A2%D7%A3/22?_=1646950864870
+
+module.exports = [
+  { id: 'channel/UCMgmeSgiQrVkYdJwl2aNe7Q' },
+  { id: 'channel/UCIld0affiSkmp-KkEit3S_w' },
+  { id: 'channel/UC29ju8bIPH5as8OGnQzwJyA' },
+  { id: 'channel/UCW5YeuERMmlnqo4oq8vwUpg' },
+  { id: 'c/programmingwithmosh' },
+  { id: 'c/Freecodecamp' }
+];
